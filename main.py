05487343059f5702f094df4159e8adf5d9b5e661
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2021-10-14 17:20:56 +0330

    merge result

--- main.py
+++ main.py
@@ -25,3 +25,21 @@ def Program(srt):
         else:
             lines[-1].append(sentence)
     lines = lines[1:]
+    subs = {start_time: line for start_time, line in zip(start_times, lines)}
+
+    with open('output.csv', 'a+', encoding='cp1252') as output:
+        writer = csv.writer(output)
+        for key, value in subs.items():
+            writer.writerow([key, value])
+    csv_file = csv.reader(open('output.csv', "r"), delimiter=",")
+    output.close()
+    with open('result.csv', mode='a+', encoding='cp1252') as result:
+        result = csv.writer(result, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        for row in csv_file:
+            for x in range(len(words)):
+                if any(words[x] in s for s in row):
+                    row.append(words[x])
+                    row.append(str(srt).replace(
+                        'D:\\03-My movies\\02-EnSubtitle\ ', ''))
+                    result.writerow(row)
