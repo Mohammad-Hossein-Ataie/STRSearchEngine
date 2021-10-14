6f33a1dc95ea5fd31357868076842bca3cf8f708
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2021-10-14 11:59:11 +0330

    a function to open srt fiels

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from pathlib import Path
 print("Well come!")
 print("Enter any word you want:")
 words = input().split(",")
+
+
+def Program(srt):
+    with open(srt, "r", encoding='cp1252') as h:
+        sub = h.readlines()
+    re_pattern = r'[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} -->'
+    regex = re.compile(re_pattern)
