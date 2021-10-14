bc891deaf37aa454e2df52eff7a0864839d26f8d
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2021-10-14 12:03:18 +0330

    store timing and lines

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ def Program(srt):
         sub = h.readlines()
     re_pattern = r'[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} -->'
     regex = re.compile(re_pattern)
+    # Get start times
+    start_times = list(filter(regex.search, sub))
+    start_times = [time.split(' ')[0] for time in start_times]
+    # Get lines
+    lines = [[]]
+    for sentence in sub:
+        if re.match(re_pattern, sentence):
+            lines[-1].pop()
+            lines.append([])
+        else:
+            lines[-1].append(sentence)
+    lines = lines[1:]
